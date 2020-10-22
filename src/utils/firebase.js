ab628b14495738c83b5d1daa5de555acfe210fa5
secrojas <sec.rojas@gmail.com>
2020-10-21 21:05:10 -0300

    deploy v1

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -12,6 +12,16 @@ const app = firebase.initializeApp({
     measurementId: "G-F2MNZYBRV0"
 });
 
+// const app = firebase.initializeApp({
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//     projectId: "secrojas-coder",
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+//   });
+
 export function getFirebase()
 {
     return app;    
