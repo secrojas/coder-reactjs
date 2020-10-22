3cbfc2b1fd65fa33079f8f449378c1c5fe3de8ab
secrojas <sec.rojas@gmail.com>
2020-10-22 12:45:00 -0300

    ajustes de compilacion

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -12,6 +12,7 @@ const app = firebase.initializeApp({
     measurementId: "G-F2MNZYBRV0"
 });
 
+
 // const app = firebase.initializeApp({
 //     apiKey: process.env.REACT_APP_API_KEY,
 //     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
