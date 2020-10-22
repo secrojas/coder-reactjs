4aa95536e7bdc6e09c42a557d5cf83c5a10ff563
secrojas <sec.rojas@gmail.com>
2020-10-22 16:35:02 -0300

    deploy v4

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,6 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// const app = firebase.initializeApp({
-//     apiKey: "AIzaSyA7oNh1OvDKLlKckEJpFwswy2kg_aULyCg",
-//     authDomain: "secrojas-coder.firebaseapp.com",
-//     databaseURL: "https://secrojas-coder.firebaseio.com",
-//     projectId: "secrojas-coder",
-//     storageBucket: "secrojas-coder.appspot.com",
-//     messagingSenderId: "1026006815729",
-//     appId: "1:1026006815729:web:92bc2f7bb573fe86182a50",
-//     measurementId: "G-F2MNZYBRV0"
-// });
-
 const app = firebase.initializeApp({
     apiKey: "process.env.REACT_APP_API_KEY",
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
